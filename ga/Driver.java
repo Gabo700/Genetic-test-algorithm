3071ee1bc52c1f5067edde2895420ea3d677e835
Gabriel Ribeiro <82044329+Gabo700@users.noreply.github.com>
2023-02-27 00:18:30 -0300

    Update Driver.java

--- ga/Driver.java
+++ ga/Driver.java
@@ -1,14 +1,23 @@
 package genetic;
 
 import java.util.Arrays;
-
 public class Driver {
 	public static void main(String[]args) {
 		Population population =  new Population(GeneticAlgorithm.POPULATION_SIZE).initializePopulation();
+		GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
 		System.out.println("-----------------------");
 		System.out.println("Generation # 0 " + " | Fittest chromosome fitness: " + population.getChromosomes()[0].getFitness());
 		printPopulation(population, "Target Chromosome: "+Arrays.toString(GeneticAlgorithm.TARGET_CHROMOSOME));
-	}
+		int generationNumber = 0;
+		while (population.getChromosomes()[0].getFitness() < GeneticAlgorithm.TARGET_CHROMOSOME.length) {
+			generationNumber++;
+			System.out.println("\n-----------------------");
+			population = geneticAlgorithm.evolve(population);
+			population.sortChomosomesByFitness();
+			System.out.println("Generation #"+generationNumber+" | Fittest chromosome fitness: "+
+			population.getChromosomes()[0].getFitness());
+			printPopulation(population, "Target Chromosome: "+Arrays.toString(GeneticAlgorithm.TARGET_CHROMOSOME));
+	}}
 	public static void printPopulation(Population population, String heading) {
 		System.out.println(heading);
 		System.out.println("----------------------------------");
